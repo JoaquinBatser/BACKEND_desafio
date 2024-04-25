c7bafb3c97f273c91d5111382b7ce32961ace4e9
Joaquin Batista <jbatistaservetti@gmail.com>
2024-04-25 18:38:45 -0300

    desafio14

--- desafio14/server/src/test/supertest.test.js
+++ desafio14/server/src/test/supertest.test.js
@@ -6,32 +6,41 @@ const requester = supertest('http://localhost:8000')
 
 describe('Testing e-commerce', () => {
   describe('Testing login - register', () => {
-    it('REGISTER', async() => {
-      
+    let cookie
+    it('REGISTER', async () => {
       const user = {
-        first_name: "John",
-        last_name: "Doe",
-        email: "johndoe@email.com",
-        password: "123",
+        first_name: 'John',
+        last_name: 'Doe',
+        email: 'johndoe2@email.com',
+        password: '123',
         age: 33,
-        role: "premium"
+        role: 'premium',
       }
 
       const response = await requester.post('/api/sessions/signup').send(user)
-    
+      expect(response._body.success).to.be.equal(true)
     })
-    it('LOGIN', async() => {
-
+    it('LOGIN', async () => {
       const user = {
-        email: "johndoe@email.com",
-        password: "123"
-      } 
+        email: 'johndoe@email.com',
+        password: '123',
+      }
 
       const response = await requester.post('/api/sessions/login').send(user)
-      const cookie = response.headers('set-cookie'[0])
-      expect(response.statusCode).to.be.equal(200)
+      const cookieResult = response.headers['set-cookie'][0]
+      expect(cookieResult).to.be.ok
+      cookie = {
+        name: cookieResult.split('=')[0],
 
+        value: cookieResult.split('=')[1].split(';')[0],
+      }
+      expect(cookie.name).to.be.equal('connect.sid')
+    })
+    it('CURRENT USER', async () => {
+      const { _body } = await requester
+        .get('/api/sessions/current')
+        .set('Cookie', [`${cookie.name}=${cookie.value}`])
+      expect(_body.user.user.email).to.be.equal('johndoe@email.com')
     })
   })
-  
 })
